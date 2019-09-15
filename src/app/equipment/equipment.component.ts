2826dee48b01868ee16c290bdd267b2a6beaa176
Michael Sisay <ashenafimichael25@gmail.com>
2019-09-15 11:40:19 -0500

    done with bonus

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
-
-  constructor() { }
+ equipments = ["Habitat dome,","Drones", "Food containers","Oxygen tanks"];
+ beingEdited :string;
+ constructor() { }
 
   ngOnInit() {
   }
-
+ add(equip:string){
+   if( !this.equipments.includes(equip.trim()) && equip.trim().length>0 ){
+     this.equipments.push(equip);
+   }
+  }
+  remove(str:string){
+   this.equipments.splice(this.equipments.indexOf(str),1);
+  }
+  edit(str:string){
+    this.beingEdited = str;
+  }
+  changes(str:string, str2:string){
+    if( !this.equipments.includes(str) && str.trim().length>0){
+      this.equipments[this.equipments.indexOf(str2)] = str;
+    this.beingEdited=null;
+    }
+}
 }
