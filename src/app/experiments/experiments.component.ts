a00fbade882fb54018adbbe04df71aa7c17f16fd
Michael Sisay <ashenafimichael25@gmail.com>
2019-09-15 11:38:35 -0500

    done with bonus

--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiments.component.css']
 })
 export class ExperimentsComponent implements OnInit {
-
-  constructor() { }
+ experiments :string[] =["Mars soil sample","Plant growth in habitat","Human bone density"];
+ selected:string; 
+ showWarning:boolean=false;
+ constructor() { }
 
   ngOnInit() {
+  }
+  add(str:string){
+    if(!this.experiments.includes(str.trim()) && str.trim() !==""){
+      this.experiments.push(str);
+    }
+  }
+  edit(str:string){
+   this.selected = str;
+   this.showWarning =false;
+  }
+  change(str:string,str2:string){
+    if(!this.experiments.includes(str2.trim()) && str2.trim()!== ""){
+     this.experiments[this.experiments.indexOf(str)] = str2;
+     this.selected ='';
+     this.showWarning =false;
+    }else{
+    this.showWarning =true;
+    }
   }
 
 }
