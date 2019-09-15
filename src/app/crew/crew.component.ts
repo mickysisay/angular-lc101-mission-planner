1ea6ebde0184c79de6e133ff773acc9bd8341e3d
Michael Sisay <ashenafimichael25@gmail.com>
2019-09-14 22:53:48 -0500

    done

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -6,16 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crew.component.css']
 })
 export class CrewComponent implements OnInit {
-
-  crew: object[] = [
+  editingName:boolean[]=[true,true,true];
+  crew: any[] = [
     {name: "Eileen Collins", firstMission: false},
     {name: "Mae Jemison", firstMission: false},
     {name: "Ellen Ochoa", firstMission: true}
   ];
-
+ editing:boolean=false;
+  chosenOne:object={name: "Eileen Collins", firstMission: false};
   constructor() { }
 
   ngOnInit() {
   }
-
+  isFirst(obj: any){
+    if(obj.firstMission === true){
+      return '-1st';
+    }
+  }
+  add(newCrew:string,isFirst:boolean){
+    this.crew.push({"name": newCrew , "firstMission":isFirst});
+    this.editingName.push(true);
+  }
+  remove(obje:object){
+    this.crew.splice(this.crew.indexOf(obje),1);
+    this.editingName.splice(this.crew.indexOf(obje),1);
+  }
+ timeToEdit(nameobj:any){
+   return this.editingName[this.crew.indexOf(nameobj)];
+ }
+ actuallyEdit(nameobj:any){
+   for(let i:number = 0;i<this.editingName.length;i++){
+     if(i===this.crew.indexOf(nameobj)){
+      this.editingName[this.crew.indexOf(nameobj)]=this.editingName[this.crew.indexOf(nameobj)] === false ? true: false;
+     }else{
+      this.editingName[i]=true;
+     }
+   }
+  
+  
+ }
+  changeName(nameobj: any ,newName:string){
+  this.crew[this.crew.indexOf(nameobj)].name = newName;
+ }
 }
